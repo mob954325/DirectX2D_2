e7a98f8b7bf0b8f32675ce9430960963ed57a433
mob954325 <lsh954325@gmail.com>
2025-06-30 17:40:11 +0900

    스프라이트, 애니메이션 데이터 구조체 추가

---
+++ Engine/Resources/AnimationResource.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "pch.h"
+
+struct FrameInfo
+{
+	std::wstring sprite;	// Sprite 이름
+	int spriteSheetIndex;	// Sprite 인덱스
+	float duration;			// 프레임 지속시간
+};
+
+struct AnimationClip 
+{
+	std::wstring clipName;			// 애니메이션 클립 이름
+	std::wstring texturePath;		// 애니메이션 클립에 사용되는 텍스처 경로
+	bool isLoop;					// 애니메이션 루프 여부
+	float duration;					// 애니메이션 전체 실행 시간
+	std::vector<FrameInfo> frames;	// 프레임 데이터
+};
---
+++ Engine/Resources/SpriteResource.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "pch.h"
+#include <unordered_map>
+
+struct Sprite
+{
+	std::wstring name;		// sprite 이름
+	float x, y;				// sprite 좌표 ( sourceRect 위치 값으로 추정 )
+	float width, height;	// sprite 너비
+	float pivotX, pivotY;	// sprite 피벗 ( 중심점 )
+};
+
+struct SpriteSheet 
+{
+	std::wstring sheetName;			// 해당 시트 이름
+	float width, height;			// 해당 시트 너비와 높이
+	std::vector<Sprite> sprites;	// 시트에 있는 Sprite 목록
+	std::unordered_map<std::wstring, int> spriteIndexMap;	// std::wstring으로 찾기 위한 인덱스 맵 <이름, 인덱스>
+};
