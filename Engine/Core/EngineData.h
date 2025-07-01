9102c72b041546bbd91bddee1b7438b08bf196c1
Lee SeongHo <87255621+mob954325@users.noreply.github.com>
2025-07-01 11:18:35 +0900

    Update EngineData.h
    창 제목 변경

--- Engine/Core/EngineData.h
+++ Engine/Core/EngineData.h
@@ -4,7 +4,7 @@
 namespace EngineData
 {
 	const static std::wstring WindowName = L"WindowClass";
-	const static std::wstring TitleName = L"DirectX11 2D Text Project";
+	const static std::wstring TitleName = L"DirectX11 2D Resource Test Project";
 
 	const static int SceenHeight = 768;
 	const static int SceenWidth = 1024;
