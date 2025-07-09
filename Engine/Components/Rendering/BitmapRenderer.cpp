e61158c576d13ffddf79eebca0a0b305067261ed
mob954325 <lsh954325@gmail.com>
2025-07-09 15:37:01 +0900

    불필요한 주석 제거

--- Engine/Components/Rendering/BitmapRenderer.cpp
+++ Engine/Components/Rendering/BitmapRenderer.cpp
@@ -21,10 +21,7 @@ void BitmapRenderer::Render(D2DRenderManager* manager)
 
 void BitmapRenderer::OnStart()
 {
-	// 유니티 좌표 매트릭스를 위한 스크린크기 불러오기
-	// screenWidth = EngineData::SceenWidth;
-	// screenHeight = EngineData::SceenHeight;
-	// unityCoordMatrix = D2D1::Matrix3x2F::Scale(1.0f, -1.0f) * D2D1::Matrix3x2F::Translation((FLOAT)(screenWidth / 2), (FLOAT)(screenHeight / 2));
+
 }
 
 void BitmapRenderer::OnDestroy()
@@ -37,38 +34,7 @@ void BitmapRenderer::CreateBitmapResource(std::wstring filePath)
 	m_bitmapResource = resourceManager->CreateBitmapResource(filePath);
 }
 
-//void BitmapRenderer::SetRenderAnchor(float x, float y)
-//{
-//	offsetX = x; offsetY = y;
-//	
-//	normalRenderMatrix = D2D1::Matrix3x2F::Scale(1.0f, 1.0) * D2D1::Matrix3x2F::Translation(offsetX, -offsetY);
-//	unityRenderMatrix = D2D1::Matrix3x2F::Scale(1.0f, -1.0f) * D2D1::Matrix3x2F::Translation(offsetX, offsetY);
-//}
-
 std::shared_ptr<BitmapResource> BitmapRenderer::GetResource()
 {
 	return m_bitmapResource;
 }
-
-//void BitmapRenderer::CalculateFinalMatrix() // -> ??
-//{
-//	Camera* pCam = Singleton<SceneManager>::GetInstance().GetMainCamera();
-//	D2D1_MATRIX_3X2_F mainCamInvertMatrix = pCam ? pCam->GetInvertMatrix() : D2D1::Matrix3x2F::Identity();
-//	
-//	// 최종 변환 값 계산
-//	if (owner->transform->IsUnityCoords())
-//	{
-//		finalMatrix =
-//			unityRenderMatrix *					// Render Matrix
-//			owner->transform->ToWorldMatrix() *	// m_transform world matrix 
-//			mainCamInvertMatrix *				// MainCamera invert matrix
-//			unityCoordMatrix;					// unity coord Matrix
-//	}
-//	else
-//	{
-//		finalMatrix =
-//			normalRenderMatrix *				// Render Matrix
-//			owner->transform->ToWorldMatrix() *	// m_transform world matrix 
-//			mainCamInvertMatrix;				// MainCamera invert matrix	
-//	}
-//}
